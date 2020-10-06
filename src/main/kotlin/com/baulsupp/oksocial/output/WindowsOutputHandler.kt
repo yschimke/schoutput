1a19bcd6f5bb45e2a592fc9230eb422508d15703
Yuri Schimke <yuri@schimke.ee>
2020-10-06 20:31:54 +0100

    Windows file open

--- src/main/kotlin/com/baulsupp/oksocial/output/WindowsOutputHandler.kt
+++ src/main/kotlin/com/baulsupp/oksocial/output/WindowsOutputHandler.kt
@@ -1,5 +1,11 @@
 package com.baulsupp.oksocial.output
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okio.ByteString.Companion.toByteString
+import java.awt.Desktop
+import java.io.File
+
 class WindowsOutputHandler<R>(responseExtractor: ResponseExtractor<R>): ConsoleHandler<R>(responseExtractor) {
   override suspend fun openLink(url: String) {
     val result = execResult("rundll32", "url.dll,FileProtocolHandler", url, outputMode = ConsoleHandler.Companion.OutputMode.Hide)
@@ -8,8 +14,19 @@ class WindowsOutputHandler<R>(responseExtractor: ResponseExtractor<R>): ConsoleH
       throw UsageException("open url failed: $url")
     }
   }
+
+  override suspend fun openPreview(response: R) {
+    withContext(Dispatchers.IO) {
+      if (Desktop.isDesktopSupported()) {
+        val tempFile = writeToFile(response)
+
+        execResult("powershell", "-c", tempFile.path)
+      }
+    }
+  }
 }
 
 suspend fun main() {
-  WindowsOutputHandler(SimpleResponseExtractor).openLink("https://www.google.com")
+  WindowsOutputHandler(SimpleResponseExtractor).openPreview(
+    SimpleResponse("image/png", source = File("src/test/resources/PNG_transparency_demonstration_1.png").readBytes().toByteString()))
 }
